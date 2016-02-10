5cd154a261d14c77384e455b5710b08617809cfd
suhk <kennethgenesuh@gmail.com>
2016-02-10 12:55:31 -0800

    Clock uses angular

--- kennysuh/app/js/client.js
+++ kennysuh/app/js/client.js
@@ -1,4 +1,11 @@
-setInterval(() => {
-  $('#date').empty();
-  $('#date').append(new Date());
-}, 1000);
+var angular = require('angular');
+
+const clockApp = angular.module('clockApp', []);
+clockApp.controller('clockController', ['$scope', '$interval', ($scope, $interval) => {
+  $interval(function() {
+    var date = new Date();
+    date.setUTCHours(date.getUTCHours() + $scope.timeZone);
+    $scope.time = date.toUTCString();
+  }, 1000);
+
+}]);
